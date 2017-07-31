a8f98f8264c6951ab9bc49c4849f32be68d074f3
hominh <minhhh12@gmail.com>
2017-07-31 22:13:26 +0700

    Category module

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Requests;
+use Illuminate\Http\Request;
 use App\Category;
 use DB;
 use Auth;
@@ -11,6 +11,11 @@ use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -18,7 +23,12 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $data = DB::table('categories')
+                    ->select(DB::raw("categories.id,categories.name,(CASE WHEN (status = 1) THEN 'Actived' ELSE 'Disable' END) as status,categories.created_at,users.name as uname"))
+                    ->leftJoin('users','categories.user_id','=','users.id')
+                    ->get();
+
+        return view('admin/category/list',compact('data'));
     }
 
     /**
@@ -40,7 +50,30 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required|unique:categories|min:3',
+        ]);
+
+
+        $hassub = 1;
+        $category = new Category;
+        $category->name = $request->name;
+        $category->alias = changeTitle($request->name);
+        $category->parent_id = $request->parent;
+        $category->title = $request->title;
+        $category->keyword = $request->keyword;
+        $category->description = $request->description;
+        $category->status = $request->status;
+        $category->user_id = Auth::user()->id;
+        if($request->parent != 0) {
+            $parentcategory = Category::find($request->parent);
+            $parentcategory->hassub = $hassub;
+            $parentcategory->save();
+        }
+
+        $category->hassub = 0;
+        $category->save();
+        return redirect()->route('admin.category.list');
     }
 
     /**
